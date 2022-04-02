9f25f323cec4d5b1f2946d10a2de4d5aa50c74ef
Xiangyu Wan <39484269+JohnnyXiangyu@users.noreply.github.com>
2022-04-02 11:13:07 -0700

    setting up archive document script

--- Assets/Scripts/Archive System/ArchiveDocument.cs
+++ Assets/Scripts/Archive System/ArchiveDocument.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArchiveDocument : MonoBehaviour
+[CreateAssetMenu(fileName = "Archive Document", menuName = "ScriptableObjects/ArchiveDocument")]
+public class ArchiveDocument : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string documentName = "";
+    public string documentText = "";
 }
